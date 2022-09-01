e2a3c2ad7edd53f45ed4b67c07cef0be072143b7
a1833554 <110015029+a1833554@users.noreply.github.com>
2022-09-01 12:08:16 +0930

    Add files via upload

--- Capybara.cpp
+++ Capybara.cpp
@@ -1,3 +1,23 @@
 #include "Capybara.h"
+using namespace std;
 
-Capybara::Capybara(){}
+
+
+void Capybara::setName(string sname)
+{
+    name = sname;
+}
+
+string Capybara::getName()
+{
+    return name;
+} 
+void Capybara::setAge(int sage)
+{
+    age = sage;
+}
+int Capybara::getAge()
+{
+    return age;
+}
+
--- Capybara.h
+++ Capybara.h
@@ -1,23 +1,18 @@
-
+#ifndef CAPYBARA_H
 #define CAPYBARA_H
 #include <string>
+#include <iostream>
 using namespace std;
+
 
 class Capybara{
     public:
     string name;
     int age;
                    // no name or age is required to create a Capybara
-void setName(string capyName){
-name = capyName;
-}  // change the capybara's name
-string getName(){
-return name;
-}
-void setAge(int capyAge){ 
-age = capyAge; 
-}      // change the capybara's age
-int getAge(){
-return age;
-}
+void setName(string capyName);  // change the capybara's name
+string getName();
+void setAge(int capyAge);     // change the capybara's age
+int getAge();
 };
+#endif
